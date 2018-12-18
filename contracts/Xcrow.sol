06a0be296d3e99abef7864cb0cc85f70069715d6
Chris Lumpkin <chrislumpkin@Chriss-MacBook-Pro.local>
2018-12-18 16:32:11 -0500

    Fleshed out createEscrow, added finder function

--- contracts/Xcrow.sol
+++ contracts/Xcrow.sol
@@ -35,6 +35,7 @@ contract Xcrow is Initializable, Ownable, Pausable {
         uint256 amount;
         uint256 endTimestamp;
         uint256 creationTimestamp;
+        uint256 id;
     }
 
     ERC20 public ERC20Interface;
@@ -45,7 +46,7 @@ contract Xcrow is Initializable, Ownable, Pausable {
     // Iterable list of escrow depositors
     address[] public escrowDepositorList;
 
-    event EscrowSucceeded(address indexed _depositor, address indexed _recipient, uint256 _amount);
+    event EscrowSucceeded(uint256 indexed _id, address indexed _depositor, address indexed _recipient, uint256 _amount);
     event EscrowFailed(address indexed _depositor, address indexed _recipient, uint256 _amount);
 
     /**
@@ -57,52 +58,93 @@ contract Xcrow is Initializable, Ownable, Pausable {
     *
     */
     function initialize() public initializer {
-        //
+        // TODO is this needed?
     }
 
     /**
     *
     */
     function createDepositorEscrow(
+        address _tokenContract,
         address _recipient,
-        uint256 _amount)
-    public whenNotPaused returns(uint256) {
-        //
+        uint256 _amount
+    ) public whenNotPaused {
+        // TODO validation
+
+        createEscrow(
+            msg.sender,
+            _recipient,
+            _tokenContract,
+            0x0,
+            EscrowTypes.DepositorAcceptance,
+            _amount,
+            0
+        );
     }
 
     /**
     *
     */
-    function createTimeBasedEscrow(address _recipient, uint256 _amount, uint256 _endTimestamp) public whenNotPaused {
-        //
+    function createTimeBasedEscrow(
+        address _tokenContract,
+        address _recipient,
+        uint256 _amount,
+        uint256 _endTimestamp
+    ) public whenNotPaused {
+        // TODO validation
+
+        createEscrow(
+            msg.sender,
+            _recipient,
+            _tokenContract,
+            0x0,
+            EscrowTypes.TimeBased,
+            _amount,
+            _endTimestamp
+        );
     }
 
     /**
     *
     */
-    function createThirdPartyEscrow(address _recipient, uint256 _amount, address _thirdParty) public whenNotPaused {
-        //
+    function createThirdPartyEscrow(
+        address _tokenContract,
+        address _recipient,
+        uint256 _amount,
+        address _thirdParty
+    ) public whenNotPaused {
+        // TODO validation
+
+        createEscrow(
+            msg.sender,
+            _recipient,
+            _tokenContract,
+            _thirdParty,
+            EscrowTypes.ThirdPartyAcceptance,
+            _amount,
+            0
+        );
     }
 
     /**
     *
     */
-    function confirmEscrow(address _recipient) whenNotPaused public {
-        //
+    function confirmEscrow(address _recipient, uint256 _id) public whenNotPaused {
+        // TODO implement
     }
 
     /**
     *
     */
-    function cancelEscrow(address _recipient) whenNotPaused public {
-        //
+    function cancelEscrow(address _recipient, uint256 _id) public whenNotPaused {
+        // TODO implement
     }
 
     /**
     * @dev Dump all escrow accounts for depositor
     */
-    function dumpEscrow(address _owner) public onlyOwner {
-        //
+    function cancelAllEscrowsForDepositor(address _depositor) public onlyOwner {
+        // TODO implement
     }
 
     /**
@@ -117,7 +159,9 @@ contract Xcrow is Initializable, Ownable, Pausable {
         uint256 _amount,
         uint256 _endTimestamp)
     private whenNotPaused {
+        // TODO test
 
+        // validations
         require(_amount > 0, "Amount must be > 0");
 
         // initialize token contract
@@ -129,11 +173,15 @@ contract Xcrow is Initializable, Ownable, Pausable {
             revert("Xcrow is not authorized to transfer the amount specified");
         }
 
+        // only executed if transfer was successful
         if(ERC20Interface.transferFrom(_depositor, address(this), _amount)) {
-            // only executed if transfer was successful
 
-            // add index
-            escrowDepositorList.push(_depositor);
+            uint256 id = escrow[_depositor].length;
+
+            if(escrow[_depositor].length == 0) {
+                // add depositor to list if this is their first escrow
+                escrowDepositorList.push(_depositor);
+            }
 
             // add escrow data
             escrow[_depositor].push(
@@ -144,13 +192,30 @@ contract Xcrow is Initializable, Ownable, Pausable {
                     _escrowType,
                     _amount,
                     _endTimestamp,
-                    now)
+                    now,
+                    id)
             );
 
-            emit EscrowSucceeded(_depositor, _recipient, _amount);
+            emit EscrowSucceeded(id, _depositor, _recipient, _amount);
 
         } else {
             emit EscrowFailed(_depositor, _recipient, _amount);
+        }
+    }
+
+    /**
+    * @dev Escrow finder function
+    */
+    function findEscrow(
+        address _depositor,
+        address _recipient,
+        uint256 _id
+    ) private view returns(Escrow) {
+
+        for(uint i = 0; i < escrow[_depositor].length; i++) {
+            if(escrow[_depositor][i].recipient == _recipient && escrow[_depositor][i].id == _id) {
+                return escrow[_depositor][i];
+            }
         }
     }
 }
